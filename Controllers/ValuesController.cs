0e500e7a6e3b92c37594357950f45145fd5a0223
Stuart Huggins <huggins@tuta.io>
2020-07-24 03:26:13 +0100

    created two async endpoints in ValuesController

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetworkApp.API.Data;
+
+namespace NetworkApp.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ValuesController : ControllerBase
+  {
+    private readonly DataContext _context;
+
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetValues()
+    {
+      var values = await _context.Values.ToListAsync();
+      return Ok(values);
+    }
+
+    [Route("{id}")]
+    [HttpGet]
+    public async Task<IActionResult> GetValue(int id)
+    {
+      var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+      return Ok(value);
+    }
+  }
+}
